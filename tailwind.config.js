decf9cc2eb3d770b95ca109e68c576f133c03c45
larrobbal <robb.strike@gmail.com>
2024-05-26 16:45:52 -0600

    Primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}",
+  "./node_modules/tw-elements/js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("tw-elements/plugin.cjs")],
+  darkMode: "class"
+}
